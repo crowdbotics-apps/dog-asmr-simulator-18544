08f3c311cf122b8f0809b9bbaf7dc83aaa84dc7e
Crowdbotics <team@crowdbotics.com>
2020-07-02 13:43:00 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/dog_asmr_simulator_18544/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dog_asmr_simulator_18544;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "dog_asmr_simulator_18544";
+    }
+}
